7b850b02753ce04e55f7383d42206794dfa0d91c
Appeyron <jens.george@outlook.de>
2026-05-19 21:05:39 +0200

    step 2 - ASCII art Stage 0

--- snowman.py
+++ snowman.py
@@ -4,19 +4,67 @@ import random
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
 
+# Snowman ASCII Art stages
+STAGES = [
+    """
+     ___
+    /___\\
+    (o o)
+    ( : )
+    ( : )
+    """,
+    """
+     ___
+    /___\\
+    (o o)
+    ( : )
+    """,
+    """
+     ___
+    /___\\
+    (o o)
+    """,
+    """
+     ___
+    /___\\
+    """
+]
+
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    """Display the current snowman stage and hidden word."""
+    print(STAGES[mistakes])
+
+    display_word = ""
+
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+
+    print("Word:", display_word)
+    print()
+
+
 def get_random_word():
-    """Selects a random word from the list."""
+    """Select a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
 
 def play_game():
+    """Start the Snowman Meltdown game."""
     secret_word = get_random_word()
-    print("Welcome to Snowman Meltdown!")
-    print("Secret word selected: " + secret_word)  # for testing, later remove this line
+    guessed_letters = []
+    mistakes = 0
 
-    # TODO: Build your game loop here.
-    # For now, simply prompt the user once:
+    print("Welcome to Snowman Meltdown!")
+
+    display_game_state(mistakes, secret_word, guessed_letters)
+
     guess = input("Guess a letter: ").lower()
+    guessed_letters.append(guess)
+
     print("You guessed:", guess)
 
 
